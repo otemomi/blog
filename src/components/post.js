e27b4ddceefc56b8d8b93e4a5f7ee8ec7c5efee6
otemomi <to.8581.mn@gmail.com>
2019-10-21 23:11:47 +0900

    Update post.js

--- src/components/post.js
+++ src/components/post.js
@@ -60,7 +60,12 @@ const Post = ({
         ) : (
           <>
             <div dangerouslySetInnerHTML={{ __html: html }} />
-            
+            <Navigation
+              previousPath={previousPath}
+              previousLabel={previousLabel}
+              nextPath={nextPath}
+              nextLabel={nextLabel}
+            />
           </>
         )}
       </div>
